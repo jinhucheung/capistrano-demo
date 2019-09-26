c1a50207aa49c8f1c45f34550c21897aed4988aa
jimcheung <hi.jinhu.zhang@gmail.com>
2019-09-26 16:54:23 +0800

    update capistrano puma config

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,6 +36,13 @@ set :default_env, {
   node_env: :production
 }
 
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_conf, "#{shared_path}/puma.rb"
+
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
 
